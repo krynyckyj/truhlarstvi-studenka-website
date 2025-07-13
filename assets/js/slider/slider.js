5a36db97b677ad51905817626c0c8d04d124eefd
{krynyckyj} <{kosmopakowastaken@gmail.com}>
2025-07-13 13:01:50 +0200

    Added slider logic, not scrolling past last element

--- assets/js/slider/slider.js
+++ assets/js/slider/slider.js
@@ -4,8 +4,25 @@
 
     arrow.addEventListener("click", () => {
       track.scrollBy({
-        left: 300, 
+        left: 280, 
         behavior: "smooth",
       });
     });
   });
+
+  function updateScrollbar() {
+  const scrollLeft = track.scrollLeft;
+  const scrollWidth = track.scrollWidth - track.clientWidth;
+  const progress = (scrollLeft / scrollWidth) * 100;
+  thumb.style.width = `${progress}%`;
+
+  if (scrollLeft >= scrollWidth - 1) {
+    arrow.disabled = true;
+    arrow.style.opacity = 0.3;
+    arrow.style.cursor = "default";
+  } else {
+    arrow.disabled = false;
+    arrow.style.opacity = 1;
+    arrow.style.cursor = "pointer";
+  }
+}
